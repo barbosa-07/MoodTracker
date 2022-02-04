44cfef82953d0b289e197dd8e7d15c8d9d4302ca
Rizwan Akram <rizwanakram4049@gmail.com>
2022-02-05 04:20:56 +0530

    feat: add functionalty to show selcted emoji

--- src/components/MoodPicker.tsx
+++ src/components/MoodPicker.tsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import { MoodOptionType } from '../types';
 
-const moodOptions = [
+const moodOptions: MoodOptionType[] = [
   { emoji: '🧑‍💻', description: 'studious' },
   { emoji: '🤔', description: 'pensive' },
   { emoji: '😊', description: 'happy' },
@@ -10,12 +11,25 @@ const moodOptions = [
 ];
 
 export const MooodPicker: React.FC = () => {
+  const [selectedMood, setSelectedMood] = React.useState<MoodOptionType>();
   return (
     <View style={styles.moodList}>
       {moodOptions.map(option => (
-        <Text key={option.emoji} style={styles.moodList}>
-          {option.emoji}
-        </Text>
+        <View key={option.emoji}>
+          <Pressable
+            onPress={() => setSelectedMood(option)}
+            style={[
+              styles.moodItem,
+              selectedMood?.emoji === option.emoji
+                ? styles.selectedMoodItem
+                : null,
+            ]}>
+            <Text style={styles.moodList}>{option.emoji}</Text>
+          </Pressable>
+          <Text style={styles.descriptionText}>
+            {option.emoji === selectedMood?.emoji ? option.description : null}
+          </Text>
+        </View>
       ))}
     </View>
   );
@@ -29,5 +43,24 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     justifyContent: 'space-between',
     padding: 20,
+  },
+  moodItem: {
+    width: 60,
+    height: 60,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 30,
+    marginBottom: 5,
+  },
+  selectedMoodItem: {
+    borderWidth: 2,
+    backgroundColor: '#454C73',
+    borderColor: '#fff',
+  },
+  descriptionText: {
+    color: '#454C73',
+    fontWeight: 'bold',
+    fontSize: 10,
+    textAlign: 'center',
   },
 });
