6da23dc6ad402eccdfbb3ca8831e4c4d0868c7ed
Rizwan Akram <rizwanakram4049@gmail.com>
2022-02-05 03:27:05 +0530

    feat : creates basic Home and HIstory component

---
+++ src/screens/Home.screen.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export const Home: React.FC = () => {
+  return (
+    <View>
+      <Text>Home Screen</Text>
+    </View>
+  );
+};
